a1000ac7cae4aef5055cc1743988bdbf6a80d208
Kr1s68 <extgreader@abv.bg>
2023-08-22 23:34:58 +0300

    Made minor changes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,32 @@
-
-import './JS-Elements/CSS-Elements/App.css';
-import MainBody from './JS-Elements/MainBody';
-import NavBar from './JS-Elements/NavBar';
+import "./JS-Elements/CSS-Elements/App.css";
+import MainBody from "./JS-Elements/MainBody";
+import NavBar from "./JS-Elements/NavBar";
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-import ExampleBuilds from './JS-Elements/ExampleBuilds';
-import SplashScreen from './JS-Elements/SplashScreen';
-import BuildYourOwn from './JS-Elements/BuildYourOwn';
-import SignUp from './JS-Elements/SignUp';
-import { AuthProvider } from './JS-Elements/Contexts/AuthContexts';
-import {auth} from "./firebase"
-import Login from "./JS-Elements/Login"
-import Dashboard from './JS-Elements/Dashboard';
-import PrivateRoute from './JS-Elements/PrivateRoute';
-import { useAuth } from './JS-Elements/Contexts/AuthContexts';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import AddCpus from './JS-Elements/addComponentElements/AddCpus';
-import AddMotherboards from './JS-Elements/addComponentElements/AddMotherboards';
-import AddGpus from './JS-Elements/addComponentElements/AddGpus';
-import AddMemory from './JS-Elements/addComponentElements/addMemory';
-import AddStorage from './JS-Elements/addComponentElements/addStorage';
-import AddCases from './JS-Elements/addComponentElements/addCases';
-import AddPsus from './JS-Elements/addComponentElements/addPsus';
-import EditorNavElement from'./JS-Elements/EditorNavElement';
-import EditExampleBuilds from './JS-Elements/EditExampleBuilds';
-import MyBuilds from './JS-Elements/MyBuilds';
-import BuildArticle from './JS-Elements/BuildArticle/BuildArticle';
-import ItemInfo from './JS-Elements/WebScrapingElements/ItemInfo';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import ExampleBuilds from "./JS-Elements/ExampleBuilds";
+import SplashScreen from "./JS-Elements/SplashScreen";
+import BuildYourOwn from "./JS-Elements/BuildYourOwn";
+import SignUp from "./JS-Elements/SignUp";
+import { AuthProvider } from "./JS-Elements/Contexts/AuthContexts";
+import { auth } from "./firebase";
+import Login from "./JS-Elements/Login";
+import Dashboard from "./JS-Elements/Dashboard";
+import PrivateRoute from "./JS-Elements/PrivateRoute";
+import { useAuth } from "./JS-Elements/Contexts/AuthContexts";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import AddCpus from "./JS-Elements/addComponentElements/AddCpus";
+import AddMotherboards from "./JS-Elements/addComponentElements/AddMotherboards";
+import AddGpus from "./JS-Elements/addComponentElements/AddGpus";
+import AddMemory from "./JS-Elements/addComponentElements/addMemory";
+import AddStorage from "./JS-Elements/addComponentElements/addStorage";
+import AddCases from "./JS-Elements/addComponentElements/addCases";
+import AddPsus from "./JS-Elements/addComponentElements/addPsus";
+import EditorNavElement from "./JS-Elements/EditorNavElement";
+import EditExampleBuilds from "./JS-Elements/EditExampleBuilds";
+import MyBuilds from "./JS-Elements/MyBuilds";
+import BuildArticle from "./JS-Elements/BuildArticle/BuildArticle";
+import ItemInfo from "./JS-Elements/WebScrapingElements/ItemInfo";
 
 //TODO: Make a login system: DONE
 //TODO: Make a Client and Admin user profiles : DONE
@@ -44,105 +38,166 @@ import ItemInfo from './JS-Elements/WebScrapingElements/ItemInfo';
 //TODO: Make a saving system for builds : DONE
 //TODO: Polish Everything : DONE
 
-export const ip = '192.168.1.4'
+export const ip = "192.168.1.4";
 
 function App() {
-
   const [isLoaded, setIsLoaded] = React.useState();
   const [currentBuild, setCurrentBuild] = React.useState();
 
-  function loadSavedBuild(Build){
-    
-    setIsLoaded(true)
+  function sendApiCall() {
+    const apiUrl = "https://pcbuilder-api.onrender.com/api/data/Cases";
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
 
-    setCurrentBuild(Build)
+    fetch(apiUrl, requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("API response:", data);
+      })
+      .catch((error) => {
+        console.error("API error:", error);
+      });
+  }
+  sendApiCall();
+  const interval = 40000;
+  setInterval(sendApiCall, interval);
 
+  function loadSavedBuild(Build) {
+    setIsLoaded(true);
+
+    setCurrentBuild(Build);
   }
 
-
   return (
-
-      <AuthProvider>
-        <Router>
+    <AuthProvider>
+      <Router>
         <Routes>
-          <Route exact path="/" element={<Home/>} />
-          <Route path='/builder' element = {<Builder isLoaded = {isLoaded} loadedBuild = {currentBuild}/>} />
-          <Route path = '/signup' element = {<SignUp/>}/>
-          <Route path = '/login' element = {<Login/>}/>
-          <Route path = '/dashboard' element = {<Dashboard/>}/>
-          <Route path = '/addcpus' element = {<> <EditorNavElement/><AddCpus/></>}/>
-          <Route path = '/addMotherboards' element = {<><EditorNavElement/><AddMotherboards/></>}/>
-          <Route path = '/addGpus' element = {<><EditorNavElement/><AddGpus/></>}/>
-          <Route path = '/addMemory' element = {<><EditorNavElement/><AddMemory/></>}/>
-          <Route path = '/addStorage' element = {<><EditorNavElement/><AddStorage/></>}/>
-          <Route path = '/addCases' element = {<><EditorNavElement/><AddCases/></>}/>
-          <Route path = '/addPsus' element = {<><EditorNavElement/><AddPsus/></>}/>
-          <Route path = '/editBuilds' element = {<EditExampleBuilds/>}/>
-          <Route path = '/mybuilds' element = {<MyBuilds loadSavedBuild = {loadSavedBuild}/>}/>
-          <Route path = '/Article0' element = {<BuildArticle index = {0} />}/>
-          <Route path = '/Article1' element = {<BuildArticle index = {1} />}/>
-          <Route path = '/Article2' element = {<BuildArticle index = {2} />}/>
-          <Route path = '/Article3' element = {<BuildArticle index = {3} />}/>
-          <Route path = '/test' element = {<ItemInfo/>}/>
+          <Route exact path="/" element={<Home />} />
+          <Route
+            path="/builder"
+            element={<Builder isLoaded={isLoaded} loadedBuild={currentBuild} />}
+          />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route
+            path="/addcpus"
+            element={
+              <>
+                {" "}
+                <EditorNavElement />
+                <AddCpus />
+              </>
+            }
+          />
+          <Route
+            path="/addMotherboards"
+            element={
+              <>
+                <EditorNavElement />
+                <AddMotherboards />
+              </>
+            }
+          />
+          <Route
+            path="/addGpus"
+            element={
+              <>
+                <EditorNavElement />
+                <AddGpus />
+              </>
+            }
+          />
+          <Route
+            path="/addMemory"
+            element={
+              <>
+                <EditorNavElement />
+                <AddMemory />
+              </>
+            }
+          />
+          <Route
+            path="/addStorage"
+            element={
+              <>
+                <EditorNavElement />
+                <AddStorage />
+              </>
+            }
+          />
+          <Route
+            path="/addCases"
+            element={
+              <>
+                <EditorNavElement />
+                <AddCases />
+              </>
+            }
+          />
+          <Route
+            path="/addPsus"
+            element={
+              <>
+                <EditorNavElement />
+                <AddPsus />
+              </>
+            }
+          />
+          <Route path="/editBuilds" element={<EditExampleBuilds />} />
+          <Route
+            path="/mybuilds"
+            element={<MyBuilds loadSavedBuild={loadSavedBuild} />}
+          />
+          <Route path="/Article0" element={<BuildArticle index={0} />} />
+          <Route path="/Article1" element={<BuildArticle index={1} />} />
+          <Route path="/Article2" element={<BuildArticle index={2} />} />
+          <Route path="/Article3" element={<BuildArticle index={3} />} />
+          <Route path="/test" element={<ItemInfo />} />
         </Routes>
       </Router>
     </AuthProvider>
-       
-     
-       
   );
-
 }
 
-function Builder(props){
+function Builder(props) {
+  return (
+    <div className="BodyDiv">
+      <NavBar />
 
-  return(
-
-          <div className='BodyDiv'>
-
-          <NavBar />
-
-          <MainBody isLoaded = {props.isLoaded} loadedBuild = {props.loadedBuild}/>
-
-          </div>
-
-  )
-
+      <MainBody isLoaded={props.isLoaded} loadedBuild={props.loadedBuild} />
+    </div>
+  );
 }
 
-function Home(){
+function Home() {
+  const [error, setError] = useState();
+  const { currentUser, logout } = useAuth();
+  const history = useNavigate();
 
-  const [error, setError] = useState()
-  const { currentUser, logout } = useAuth()
-  const history =  useNavigate()
-
-  function reDirect(){
-    if(window.innerWidth < 1350){
-      history("/builder")
+  function reDirect() {
+    if (window.innerWidth < 1350) {
+      history("/builder");
     }
   }
 
-  return(
-    
-    <div className='BodyDiv'>
+  return (
+    <div className="BodyDiv">
+      <NavBar />
 
-      <NavBar/>
+      <div className="HomeBackgroundDiv">
+        {!currentUser && <SplashScreen />}
 
-      <div className='HomeBackgroundDiv'>
+        <ExampleBuilds />
 
-        { !currentUser && <SplashScreen/>}
-
-        <ExampleBuilds/>
-
-        <BuildYourOwn/>
-
+        <BuildYourOwn />
       </div>
-
     </div>
-    
-   ) 
-  
+  );
 }
-
 
 export default App;
